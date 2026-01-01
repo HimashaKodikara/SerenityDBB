11bc6a2bb583a24b2d15d940b395d7c79273691c
19840769 <kodikarah.21@uom.lk>
2026-01-01 16:09:42 +0530

    initialize

---
+++ src/main/java/org/springdemo/seringdbb/SeringDbbApplication.java
@@ -0,0 +1,13 @@
+package org.springdemo.seringdbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeringDbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeringDbbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springdemo/seringdbb/SeringDbbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdemo.seringdbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeringDbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
