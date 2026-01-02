0586a7ea8cea829be423ab4e8f936de61a1cf98a
19840769 <kodikarah.21@uom.lk>
2026-01-02 22:17:35 +0530

    (change)updated

--- src/test/java/Tests/GoogleTest.java
+++ src/test/java/Tests/GoogleTest.java
@@ -1,34 +1,35 @@
 package Tests;
 
 import Steps.GoogleSteps;
-import io.cucumber.junit.CucumberSerenityRunner;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-@RunWith(CucumberSerenityRunner.class)
+@ExtendWith(SerenityJUnit5Extension.class)
 public class GoogleTest {
 
     @Steps
     GoogleSteps googleSteps;
 
-    @Managed(driver="chrome")
-    WebDriver drive;
+    @Managed(driver = "chrome")
+    WebDriver driver;
 
-    Properties prop=new Properties();
+    Properties prop = new Properties();
 
     @Test
     public void TestGoogle() throws IOException {
         prop.load(new FileInputStream("src/main/resources/testdata.properties"));
-       googleSteps.launchGoogleApp();
-       googleSteps.searchItem(prop.getProperty("searchItem"));
-       googleSteps.collectItem();
-       googleSteps.selectItem(prop.getProperty("selectItem"));
+
+        googleSteps.launchGoogleApp();
+        googleSteps.searchItem(prop.getProperty("searchItem"));
+        googleSteps.collectItem();
+        googleSteps.selectItem(prop.getProperty("selectItem"));
     }
 }
